1746d96bb338139dd5991ad5351e452a642f7eff
NoobMaster69 <70845529+DarkAvenger429@users.noreply.github.com>
2025-11-29 00:11:29 +0530

    update frontent

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MisinformationRadar from "./MisinformationRadar";
+
+const App: React.FC = () => {
+  return (
+    <div style={{ width: "100%", minHeight: "100vh" }}>
+      <MisinformationRadar />
+    </div>
+  );
+};
+
+export default App;
